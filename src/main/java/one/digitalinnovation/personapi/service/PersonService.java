dc265e16fbfb892230a38618d7c6c6a227fdaaeb
Breno Melo <breno-kf@hotmail.com>
2021-03-13 22:18:10 -0400

    add MessageResponseDto and change

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,27 +1,34 @@
 package one.digitalinnovation.personapi.service;
 
 
+import lombok.AllArgsConstructor;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
     private PersonRepository personRepository;
 
-    @Autowired
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
+    public MessageResponseDTO createPerson(PersonDTO personDTO){
+        Person personToSave = personMapper.toModel(personDTO);
+        Person savePerson = personRepository.save(personToSave);
+        return createMessageResponse(savePerson.getId(), "Created person with ID:: ");
     }
 
-    public MessageResponseDTO createPerson(@RequestBody Person person){
-        Person savePerson = personRepository.save(person);
+
+    private MessageResponseDTO createMessageResponse(Long id, String message) {
         return MessageResponseDTO
                 .builder()
-                .message("Created person with ID " + savePerson.getId())
+                .message(message + id)
                 .build();
     }
 }
