06302bc4ac28e9c4ff001ee3cc8bf6ece973fef3
Breno Melo <breno-kf@hotmail.com>
2021-03-14 01:04:18 -0400

    created Methode delete

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -46,10 +46,19 @@ public class PersonService {
     }
 
     public PersonDTO findById(Long id) throws PersonNotFoundException {
-       // Optional<Person> optionalPerson = personRepository.findById(id);
+        Person person = verifyIfExists(id);
+        return personMapper.toDTO(person);
+    }
+
+
+    public void delete(Long id) throws PersonNotFoundException {
+        verifyIfExists(id);
+        personRepository.deleteById(id);
+    }
+
+    private Person verifyIfExists(Long id) throws PersonNotFoundException {
         Person person = personRepository.findById(id)
                 .orElseThrow(() -> new PersonNotFoundException(id));
-
-        return personMapper.toDTO(person);
+        return person;
     }
 }
